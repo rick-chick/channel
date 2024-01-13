10f6105313de6bbddff776d56764f63da508807e
so1itaryrove <so1itaryrove@gmail.com>
2024-01-13 10:00:13 +0900

    create env local

--- src/channel/driver/env.py
+++ src/channel/driver/env.py
@@ -1,6 +1,13 @@
 DATABASE_URL = 'postgresql+pg8000://channel:password@127.0.0.1:5432/channel'
 ALLOWD_ORIGINS = '[http://127.0.0.1:4200, http://localhost:4200]'
 
-GMAIL_ACCOUNT = '***'
-GMAIL_PASSWORD = '***'
-PASSWORD_RESET_URL = '***'
+try:
+    from channel.driver.env_local import (
+        GMAIL_ACCOUNT,
+        GMAIL_PASSWORD,
+        PASSWORD_RESET_URL
+    )
+except ImportError:
+    GMAIL_ACCOUNT = '***'
+    GMAIL_PASSWORD = '***'
+    PASSWORD_RESET_URL = '***'
